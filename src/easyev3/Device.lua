1f36ac7f628eed69076e37f2399a76023f1bce88
Angus Trau <angus.amtra5@gmail.com>
2016-02-13 19:16:22 +1100

    Purged old code

---
+++ src/easyev3/Device.lua
@@ -0,0 +1,37 @@
+require "ev3dev/class.lua"
+
+local function stringSplit(inputString)
+	local output = {}
+	for i in string.gmatch(inputString, "%S+") do
+  		table.insert(output, i)
+	end
+	return output
+end
+
+local Device = class()
+
+function Device:init(name, port)
+	self._name = name
+	self._port = port
+
+	self._modes = {}
+	local rawModes = io.open("/sys/class/")
+end
+
+function Device:isConnected()
+	return self.raw:connected()
+end
+
+function Device:name()
+	return self._name
+end
+
+function Device:port()
+	return self._port
+end
+
+function Device:modes()
+	return self._modes
+end
+
+return Device
---
+++ src/easyev3/Motor/Motor.lua
@@ -0,0 +1,7 @@
+require "../ev3dev/class.lua"
+
+local Device = require "../Device.lua"
+
+local Motor = class(Device)
+
+return Motor
