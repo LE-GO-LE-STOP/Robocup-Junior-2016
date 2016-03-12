3a1122b2659e2d255c90f8327dafd787a0783c01
Angus Trau <angus.amtra5@gmail.com>
2016-03-12 12:03:19 +1100

    Floor the compass reading juts to be safe

--- src/ev3.lua
+++ src/ev3.lua
@@ -698,7 +698,7 @@ function Compass_Sensor:init(port)
 end
 
 function Compass_Sensor:direction()
-	return tonumber(self.attributes["value0"]) + 180 -- Get the value from the sensor and convert it from -180 - 180 to 0 - 360
+	return math.floor(tonumber(self.attributes["value0"]) + 180) -- Get the value from the sensor and convert it from -180 - 180 to 0 - 360
 end
 
 function Compass_Sensor:relativeDirection()
