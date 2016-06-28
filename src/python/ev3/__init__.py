31d746746c2e5b25857d9e61e8ebdc83d9dba9e0
Angus Trau <angus.amtra5@gmail.com>
2016-06-28 17:32:07 +1000

    Move constants to own file

--- src/python/ev3/__init__.py
+++ src/python/ev3/__init__.py
@@ -3,15 +3,4 @@ from .sensors import *
 from .motors import *
 from .controllers import *
 from .bluetooth import *
-
-INPUT_AUTO = ""
-INPUT_1 = "in1"
-INPUT_2 = "in2"
-INPUT_3 = "in3"
-INPUT_4 = "in4"
-
-OUTPUT_AUTO = ""
-OUTPUT_A = "outA"
-OUTPUT_B = "outB"
-OUTPUT_C = "outC"
-OUTPUT_D = "outD"
+from .constants import *
