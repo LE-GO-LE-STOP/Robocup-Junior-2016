e2322970b708b30164032bb9a0c74fad5034f16c
Angus Trau <angus.amtra5@gmail.com>
2016-06-28 16:48:29 +1000

    Added infrared sensor support

--- src/python/ev3/sensors.py
+++ src/python/ev3/sensors.py
@@ -1,6 +1,7 @@
 from ev3dev.core import TouchSensor as _TouchSensor
 from ev3dev.core import ColorSensor as _ColourSensor
 from ev3dev.core import UltrasonicSensor as _UltrasonicSensor
+from ev3dev.core import InfraredSensor as _InfraredSensor
 from math import floor
 
 class TouchSensor:
@@ -35,7 +36,7 @@ class UltrasonicSensor:
 	def __init__(self, port):
 		self._sensor = _UltrasonicSensor(port)
 
-	def distance(unit = "cm"):
+	def distance(self, unit = "cm"):
 		if unit == "cm":
 			return self._sensor.distance_centimeters()
 		else:
@@ -43,3 +44,66 @@ class UltrasonicSensor:
 
 	def nearby(self):
 		return self._sensor.other_sensor_present == 1
+
+class InfraredSensor:
+	def __init__(self, port):
+		self._sensor = _InfraredSensor(port)
+
+	def proximity(self):
+		return self._sensor.proximity()
+
+	def seek(self, channel = 1):
+		self._sensor.mode = _InfraredSensor.MODE_IR_SEEK
+
+		valueOffset = (channel - 1) * 2
+
+		return {
+			"heading": self._sensor.value(valueOffset),
+			"strength": self._sensor.value(valueOffset + 1),
+			"detected": not (heading == 0 and strength == -128),
+		}
+
+	_simpleSensorStates = [
+		{"red":{"up":False,"down":False},"blue":{"up":False,"down":False},"beacon":False}, # None
+		{"red":{"up":True,"down":False},"blue":{"up":False,"down":False},"beacon":False}}, # Red up
+		{"red":{"up":False,"down":True},"blue":{"up":False,"down":False},"beacon":False}}, # Red down
+		{"red":{"up":False,"down":False},"blue":{"up":True,"down":False},"beacon":False}}, # Blue up
+		{"red":{"up":False,"down":False},"blue":{"up":False,"down":True},"beacon":False}}, # Blue down
+		{"red":{"up":True,"down":False},"blue":{"up":True,"down":False},"beacon":False}}, # Red up, blue up
+		{"red":{"up":True,"down":False},"blue":{"up":False,"down":True},"beacon":False}}, # Red up, blue down
+		{"red":{"up":False,"down":True},"blue":{"up":True,"down":False},"beacon":False}}, # Red down, blue up
+		{"red":{"up":False,"down":True},"blue":{"up":False,"down":True},"beacon":False}}, # Red down, blue down
+		{"red":{"up":False,"down":False},"blue":{"up":False,"down":False},"beacon":True}, # Beacon
+		{"red":{"up":True,"down":True},"blue":{"up":False,"down":False},"beacon":False}}, # Red up, red down
+		{"red":{"up":False,"down":False},"blue":{"up":True,"down":True},"beacon":False}} # Blue up, blue down
+	]
+	def remote(self, channel = 1):
+		if channel == 1:
+			# Use the more detailed mode that can read multiple pressed buttons
+			self._sensor.mode = _InfraredSensor.MODE_IR_REM_A
+
+			value = self._sensor.value(0) - 0x100 # Remove the most significant byte (it is always the same)
+			buttons = {
+				"red": {
+					"up": False,
+					"down": False
+				},
+				"blue": {
+					"up": False,
+					"down": False
+				},
+				"beacon": False
+			}
+
+			if not (value & 0x0F == 0):
+				# Some buttons have been pressed
+				buttons["blue"]["down"] = value & 0x80
+				buttons["blue"]["up"] = value & 0x40
+				buttons["red"]["down"] = value & 0x20
+				buttons["red"]["up"] = value & 0x10
+				buttons["beacon"] = value & 0xF9 == 0
+
+			return buttons
+		else:
+			self._sensor.mode = _InfraredSensor.MODE_IR_REMOTE
+			return _simpleSensorStates[self._sensor.value(channel - 1)]
