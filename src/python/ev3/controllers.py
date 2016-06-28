5923cc759b15b46b4848d6f0d67f436542c07618
Angus Trau <angus.amtra5@gmail.com>
2016-06-28 17:32:19 +1000

    Update to named parameters

---
+++ src/python/ev3/controllers.py
@@ -0,0 +1,6 @@
+class Steering:
+	def __init__(self, leftMotor, rightMotor):
+		self._leftMotor = leftMotor
+		self._rightMotor = rightMotor
+
+class Tank:
--- src/python/ev3/motors.py
+++ src/python/ev3/motors.py
@@ -3,15 +3,15 @@ from time import sleep
 
 class Motor:
 	def __init__(self, port):
-		self._motor = _Motor(port)
+		self._motor = _Motor(address = port)
 		self._countPerRot = self._motor.count_per_rot
 
 	def setBrake(self, brake = False):
 		if brake:
 			self._motor.stop_action = brake
 
-	def off(self, brake = False):
-		self.setBrake(brake)
+	def off(self, brake):
+		self.setBrake(brake = brake)
 
 		self._motor.command = _Motor.COMMAND_STOP
 
@@ -19,24 +19,24 @@ class Motor:
 		self._motor.duty_cycle_sp = power
 		self._motor.command = _Motor.COMMAND_RUN_FOREVER
 
-	def onForSeconds(self, power, seconds, brake = False, wait = True):
+	def onForSeconds(self, power, seconds, brake, wait = True):
 		if wait:
-			self.on(power)
+			self.on(power = power)
 			sleep(seconds)
-			self.off(brake)
+			self.off(brake = brake)
 		else:
 			self._motor.duty_cycle_sp = power
 			self._motor.time_sp = seconds * 1000
 			self._motor.command = _Motor.COMMAND_RUN_TIMED
 
-			self.setBrake(brake)
+			self.setBrake(brake = brake)
 
-	def onForDegrees(self, power, degrees, brake = False, wait = True):
+	def onForDegrees(self, power, degrees, brake, wait = True):
 		position = degrees * (self._countPerRot / 360)
 
 		if wait:
 			targetPosition = self._motor.position + position
-			self.on(power)
+			self.on(power = power)
 
 			if degrees >= 0:
 				while self._motor.position < targetPosition:
@@ -45,13 +45,13 @@ class Motor:
 				while self._motor.position > targetPosition:
 					pass
 
-			self.off(brake)
+			self.off(brake = brake)
 		else:
 			self._motor.duty_cycle_sp = power
 			self._motor.position_sp = position
 			self._motor.command = _Motor.COMMAND_RUN_TO_REL_POS
 
-			self.setBrake(brake)
+			self.setBrake(brake = brake)
 
-	def onForRotations(self, power, rotations, brake = False, wait = True):
-		self.onForDegrees(power, rotations * 360, brake, wait)
+	def onForRotations(self, power, rotations, brake, wait = True):
+		self.onForDegrees(power = power, degrees = rotations * 360, brake = brake, wait = wait)
