b3455fb2632030d93d0fca0dfba55b18e120f8d3
Angus Trau <angustrau@users.noreply.github.com>
2016-08-04 15:53:16 +1000

    Update 04-08-2016.py

--- src/python/04-08-2016.py
+++ src/python/04-08-2016.py
@@ -1,13 +1,16 @@
+from math import pi
+
 WHEEL_CIRCUMFERENCE = 13.19
+AXLE_LENGTH = 10
 WATER_TOWER_DETECT_DISTANCE = 8
-WATER_TOWER_DETECT_ANGLE
+WATER_TOWER_DETECT_ANGLE = 40
 WATER_TOWER_DETECT_ANGLED_DISTANCE = # WATER_TOWER_DETECT_DISTANCE / cos(WATER_TOWER_DETECT_ANGLE)
 
 def detectWaterTower:
   if ultrasonicSensor.distance() < WATER_TOWER_DETECT_DISTANCE:
-    degrees = 
-    leftMotor.on(BASE_POWER, degrees, "brake", False)
-    rightMotor.on(-BASE_POWER, degrees, "brake, True)
+    degrees = (math.pi * WATER_TOWER_DETECT_ANGLE * AXLE_LENGTH) / WHEEL_CIRCUMFERENCE
+    leftMotor.onForDegrees(BASE_POWER, degrees, "brake", False)
+    rightMotor.onForDegrees(-BASE_POWER, degrees, "brake, True)
     
     distance = ultrasonicSensor.distance()
     if WATER_TOWER_DETECT_ANGLED_DISTANCE - 1 < distance and WATER_TOWER_DETECT_ANGLED_DISTANCE + 1 > distance:
