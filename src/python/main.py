9c12126bd603d6d5153b25f63e5bfce781c6bb98
Angus Trau <angus.amtra5@gmail.com>
2016-08-05 08:27:15 +1000

    Integrate changes from 4/8/16

--- src/python/main.py
+++ src/python/main.py
@@ -1,8 +1,10 @@
 import ev3
 from time import sleep
+from math import pi
 
 leftMotor = ev3.Motor("outA")
 rightMotor = ev3.Motor("outD")
+clawMotor = ev3.Motor("outB")
 
 leftSensor = ev3.ColourSensor("in1")
 rightSensor = ev3.ColourSensor("in4")
@@ -12,9 +14,10 @@ ultrasonicSensor = ev3.UltrasonicSensor("in2")
 BASE_POWER = 60
 CALIBRATED_RANGE = 10 # The margin of error that is used when checking for colour
 WHEEL_CIRCUMFERENCE = 13.573
+TURNING_DIAMETRE = pass
 WATER_TOWER_DETECT_DISTANCE = 8
-WATER_TOWER_DETECT_TURN_ANGLE = 40
-WATER_TOWER_DETECT_TURNED_DISTANCE = 10.44 # 8 / cos(40)
+WATER_TOWER_DETECT_ANGLE = 40
+WATER_TOWER_DETECT_ANGLED_DISTANCE = 10.44 # 8 / cos(40)
 
 WHITE = 0
 BLACK = 0
@@ -26,6 +29,9 @@ def withinrange(x, y, range):
 
 def iscolour(result, colour):
 	return withinrange(result, colour, CALIBRATED_RANGE)
+
+def angleToDegrees(angle):
+  return (pi * angle * AXLE_LENGTH) / WHEEL_CIRCUMFERENCE
 
 def calibrate():
 	def cal():
@@ -72,12 +78,60 @@ def detectGreenTile():
 		rightMotor.on(BASE_POWER)
 
 def detectWaterTower():
-	if ultrasonicSensor.distance() < WATER_TOWER_DETECT_DISTANCE:
-		leftMotor.onForDegrees(BASE_POWER, (), "brake", False)
-		rightMotor.onForDegrees(BASE_POWER, (), "brake", True)
+  if ultrasonicSensor.distance() < WATER_TOWER_DETECT_DISTANCE:
+    degrees = angleToDegrees(WATER_TOWER_DETECT_ANGLE)
+    leftMotor.onForDegrees(BASE_POWER, degrees, "brake", False)
+    rightMotor.onForDegrees(-BASE_POWER, degrees, "brake", True)
+    
+    distance = ultrasonicSensor.distance()
+    if WATER_TOWER_DETECT_ANGLED_DISTANCE - 1 < distance and WATER_TOWER_DETECT_ANGLED_DISTANCE + 1 > distance:
+      # Turn until 90 degrees
+    else:
+      # Reverse
+      leftMotor.on(-BASE_POWER, degrees, "brake", False)
+      rightMotor.on(BASE_POWER, degrees, "brake", True)
+      slope()
+      return
 
-def detectFinal():
-	pass
+def slope():
+  pass
+
+def detectRescueZone():
+  if iscolour(leftSensor.reflected(), SILVER) and iscolour(rightSensor.reflected(), SILVER):
+    degrees = angleToDegrees(90)
+    degree = angleToDegrees(1)
+    leftMotor.onForDegrees(-BASE_POWER, degrees, "brake", False)
+    rightMotor.onForDegrees(BASE_POWER, degrees, "brake", True)
+    
+    minDistance = 500
+    minAngle = 0
+    for i in range(0, 180):
+      leftMotor.onForDegrees(5, degree, "brake", False)
+      rightMotor.onForDegrees(5, degree, "brake", True)
+      sleep(0.5)
+      
+      distance = ultrasonicSensor.distance()
+      if distance < minDistance:
+        minDistance = distance
+        minAngle = i
+        
+    reverseMinAngle = angleToDegrees(180 - minAngle)
+    leftMotor.onForDegrees(-BASE_POWER, reverseMinAngle, "brake", False)
+    rightMotor.onForDegrees(BASE_POWER, reverseMinAngle, "brake", True)
+    
+    canRotations = minDistance / WHEEL_CIRCUMFERENCE
+    leftMotor.onForRotations(BASE_POWER, canRotations, "brake", False)
+    rightMotor.onForRotations(BASE_POWER, canRotations, "brake", True)
+    
+    clawMotor.on(100)
+    
+    leftMotor.onForRotations(-BASE_POWER, canRotations, "brake", False)
+    rightMotor.onForRotations(-BASE_POWER, canRotations, "brake", True)
+    
+    leftMotor.onForDegrees(BASE_POWER, reverseMinAngle, "brake", False)
+    rightMotor.onForDegrees(-BASE_POWER, reverseMinAngle, "brake", True)
+    
+
 
 while True:
 	detectFinal()
