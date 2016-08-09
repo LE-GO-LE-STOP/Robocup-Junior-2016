4d1c656c0855a4834dc353315ca864a5c8592d4f
Angus Trau <angus.amtra5@gmail.com>
2016-08-09 20:53:38 +1000

    Update

--- src/python/04-08-2016.py
+++ src/python/04-08-2016.py
@@ -1,94 +1,16 @@
-import ev3
-from ev3dev.ev3 import Button
-from time import sleep
 from math import pi
+from time import sleep
 
-leftMotor = ev3.Motor("outD")
-rightMotor = ev3.Motor("outA")
 clawMotor = ev3.Motor("outB")
 
-leftSensor = ev3.ColourSensor("in1")
-rightSensor = ev3.ColourSensor("in4")
-
-ultrasonicSensor = ev3.UltrasonicSensor("in2")
-
-Buttons = Button()
-
-BASE_POWER = 60
-CALIBRATED_RANGE = 10 # The margin of error that is used when checking for colour
-WHEEL_CIRCUMFERENCE = 13.573
-TURNING_DIAMETRE = 10.5
+WHEEL_CIRCUMFERENCE = 13.19
+AXLE_LENGTH = 10
 WATER_TOWER_DETECT_DISTANCE = 8
 WATER_TOWER_DETECT_ANGLE = 40
-WATER_TOWER_DETECT_ANGLED_DISTANCE = 10.44 # 8 / cos(40)
-
-WHITE = 0
-BLACK = 0
-GREEN = 0
-SILVER = 0
-
-def withinrange(x, y, range):
-	return x - range/2 <= y and x + range/2 >= y
-
-def iscolour(result, colour):
-	return withinrange(result, colour, CALIBRATED_RANGE)
+WATER_TOWER_DETECT_ANGLED_DISTANCE = # WATER_TOWER_DETECT_DISTANCE / cos(WATER_TOWER_DETECT_ANGLE)
 
 def angleToDegrees(angle):
   return (pi * angle * AXLE_LENGTH) / WHEEL_CIRCUMFERENCE
-
-def calibrate():
-	def cal():
-		ev3.Sound.playTone(500, 0.5)
-
-		while not Buttons.enter:
-			pass
-		while Buttons.enter: 
-			pass
-		return leftSensor.reflected()
-
-	print("White: ")
-	WHITE = cal()
-	print(str(WHITE))
-	print("Black: ")
-	BLACK = cal()
-	print(str(BLACK))
-	print("Green: ")
-	GREEN = cal()
-	print(str(GREEN))
-	print("Silver: ")
-	SILVER = cal()
-	print(str(SILVER))
-	print("Calibrated!")
-
-def followLine():
-	if iscolour(rightSensor.reflected(), BLACK):
-		leftMotor.on(BASE_POWER + 15)
-		rightMotor.on(BASE_POWER)
-		while iscolour(rightSensor.reflected(), BLACK):
-			pass
-		leftMotor.on(BASE_POWER)
-
-	if iscolour(leftSensor.reflected(), BLACK):
-		leftMotor.on(BASE_POWER)
-		rightMotor.on(BASE_POWER + 15)
-		while iscolour(leftSensor.reflected(), BLACK):
-			pass
-		rightMotor.on(BASE_POWER)
-
-def detectGreenTile():
-	if iscolour(leftSensor.reflected(), GREEN):
-		leftMotor.on(BASE_POWER - 30)
-		rightMotor.on(BASE_POWER + 20)
-		sleep(2)
-		leftMotor.on(BASE_POWER)
-		rightMotor.on(BASE_POWER)
-
-	if iscolour(rightSensor.reflected(), GREEN):
-		leftMotor.on(BASE_POWER + 20)
-		rightMotor.on(BASE_POWER - 30)
-		sleep(2)
-		leftMotor.on(BASE_POWER)
-		rightMotor.on(BASE_POWER)
 
 def detectWaterTower():
   if ultrasonicSensor.distance() < WATER_TOWER_DETECT_DISTANCE:
@@ -99,7 +21,6 @@ def detectWaterTower():
     distance = ultrasonicSensor.distance()
     if WATER_TOWER_DETECT_ANGLED_DISTANCE - 1 < distance and WATER_TOWER_DETECT_ANGLED_DISTANCE + 1 > distance:
       # Turn until 90 degrees
-      pass
     else:
       # Reverse
       leftMotor.on(-BASE_POWER, degrees, "brake", False)
@@ -145,16 +66,4 @@ def detectRescueZone():
     leftMotor.onForDegrees(BASE_POWER, reverseMinAngle, "brake", False)
     rightMotor.onForDegrees(-BASE_POWER, reverseMinAngle, "brake", True)
     
-calibrate()
-leftMotor.on(BASE_POWER)
-rightMotor.on(BASE_POWER)
-while True:
-	if Buttons.backspace:
-		exit()
-
-	print("Left: " + str(leftSensor.reflected()) + " Right: " + str(rightSensor.reflected()))
-
-	#detectRescueZone()
-	#detectWaterTower()
-	#detectGreenTile()
-	followLine()
+    
---
+++ src/python/motor_off.py
@@ -0,0 +1,4 @@
+import ev3
+ev3.Motor("outA").off("brake")
+ev3.Motor("outB").off("brake")
+ev3.Motor("outD").off("brake")
